b7229adc51f2a171882afd6705da15c49e35a47b
HADO564 <70658014+HADO564@users.noreply.github.com>
2021-12-20 02:22:35 +0500

    Implemented Linked Lists basic functionality into the search engine program. Created separate files to allow ease of readablity

---
+++ LinkedList.cpp
@@ -0,0 +1,143 @@
+#pragma once
+#include<iostream>
+#include "datatype.hpp"
+
+class LinkedList
+{
+    Course* first;
+    Course* last;
+    Course* loc;
+    Course* ploc;
+    int length;
+    LinkedList()
+    {
+        first=NULL;
+        last=NULL;
+        loc=NULL;
+        ploc=NULL;
+        length=0;
+    }
+    bool isEmpty()
+    {
+        return length==0;
+    }
+    void printList()
+    {
+        int x=0;
+        Course* temp=first;
+        if(!isEmpty())
+        {
+            while(temp->next!=NULL)
+            {
+                temp->PrintCourseData();
+                temp=temp->next;
+            }
+        }
+    }
+    void SearchList(std::string value)
+    {
+        loc=first;
+        ploc=NULL;
+        while(loc!=NULL && loc->data.name>value)
+        {
+            ploc=loc;
+            loc=loc->next;
+        }
+        if(loc!=NULL && loc->data.name!=value)
+        {
+            loc=NULL;
+        }
+    }
+    void insertFront()
+    {
+        Course* newCourse= new Course;
+        newCourse->InsertCourseData();
+        if(isEmpty())
+        {
+            first=newCourse;
+            last=newCourse;
+            newCourse->next=NULL;
+        }
+        else
+        {
+            newCourse->next=first;
+            first=newCourse;
+        }
+        length++;
+    }
+    void insert(std::string value)
+    {
+        Course* newCourse= new Course;
+        newCourse->InsertCourseData();
+        SearchList(value);
+        if(loc==NULL)
+        {
+            if(ploc==NULL)
+            insertFront();
+            else
+            {
+                newCourse->next=ploc->next;
+                ploc->next=newCourse;
+                if (ploc==last)
+                {
+                    last=newCourse;
+                }
+            }
+            length++;
+        }
+        else
+        {
+            std::cout<<"Duplication of courses is not allowed"<<std::endl;
+        }
+    }
+    void deleteNode(std::string value)
+    {
+        if(!isEmpty())
+        {
+            SearchList(value);
+            if(loc!=NULL)
+            {
+                if(first==last)
+                {
+                    first=NULL;
+                    last=NULL;
+                }
+                else if(ploc==NULL)
+                {
+                    first=first->next;
+                    delete loc;
+                    loc=first;
+                }
+                else if(loc->next)
+                {
+                    delete last;
+                    last=ploc;
+                    last->next=NULL;
+                }
+                else
+                {
+                    ploc->next=loc->next;
+                    delete loc;
+                }
+                length--;
+            }
+            else
+            {
+                std::cout<<"Value Not Found"<<std::endl;
+            }
+        }
+        else
+        std::cout<<"No courses are in the structure"<<std::endl;
+    }
+    void destroy()
+    {
+        Course* temp;
+        while(first!=NULL)
+        {
+            temp=first;
+            first=first->next;
+            delete temp;
+        }
+        last=NULL;
+    }
+};
---
+++ datatype.hpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <string>
+
+
+class Category
+{
+public:
+    int index;
+    std::string t_name;
+    Course *Tnext;
+};
+
+class Course_data
+{
+public:
+    std::string name;
+    std::string url;
+    int Category_id; //using h
+    int price;
+    int subscribers;
+    int course_id;
+    int publish_year;
+    float duration;
+    bool isPaid;
+};
+
+class Course
+{
+public:
+   
+    int index;
+    Course *next;  //for linked list use
+    Course *lrank; //for heap
+    Course *rrank;
+    Course_data data;
+    void PrintCourseData()//function to print the data of the course
+    {
+        std::cout<< data.name <<std::endl;
+        std::cout<< data.url<<std::endl;
+        std::cout<< data.Category_id <<std::endl;
+        std::cout<< data.price <<std::endl;
+        std::cout<< data.subscribers <<std::endl;
+        std::cout<< data.course_id <<std::endl;
+        std::cout<< data.publish_year<<std::endl;
+        std::cout<< data.duration<<std::endl;
+        std::cout<< data.isPaid<<std::endl;
+    }
+    void InsertCourseData()//function to insert the data into the course
+    {
+        std::cin>>data.name;
+        std::cin>>data.url;
+        std::cin>>data.Category_id;
+        std::cin>>data.price;
+        std::cin>>data.subscribers;
+        std::cin>>data.course_id;
+        std::cin>>data.publish_year;
+        std::cin>>data.duration;
+        if(data.price!=0)
+        data.isPaid==false;
+    }
+};
+
+
+
+class LinkedList
+{
+public:
+    Course* first;
+    Course* last;
+    int length;
+    Course* ploc;
+    Course* loc;
+    LinkedList();
+    bool isEmpty();
+    void printList();
+    void SearchList(std::string);//searches through list linearly
+    void insertFront();
+    void insert();
+    void deleteNode();
+    void destroy();//basic linked list functionalities
+};
+
+void CreateResList()//When a word is searched, a smaller linked list 
+//of the words that are hit in the search will be returned
+{
+
+}
