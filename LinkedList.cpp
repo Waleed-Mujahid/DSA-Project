3b889960eff2baa6ecaf1f0608846282228ccbca
Waleed Mujahid <mmujahid.bscs20seecs@seecs.edu.pk>
2021-12-20 14:33:15 +0500

    waleed commit

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,149 +1,232 @@
 #pragma once
-#include<iostream>
+#include <iostream>
 #include "datatype.hpp"
+#include <fstream>
+#include <sstream>
 
-class LinkedList
+LinkedList::LinkedList()
 {
-    Course* first;
-    Course* last;
-    Course* loc;
-    Course* ploc;
-    int length;
-    LinkedList()
+    first = NULL;
+    last = NULL;
+    loc = NULL;
+    ploc = NULL;
+    length = 0;
+}
+bool LinkedList::isEmpty()
+{
+    return first == NULL;
+}
+void LinkedList::printList()
+{
+    int x = 0;
+    Course *temp = first;
+    if (!isEmpty())
     {
-        first=NULL;
-        last=NULL;
-        loc=NULL;
-        ploc=NULL;
-        length=0;
-    }
-    bool isEmpty()
-    {
-        return length==0;
-    }
-    void printList()
-    {
-        int x=0;
-        Course* temp=first;
-        if(!isEmpty())
+        while (temp->next != NULL)
         {
-            while(temp->next!=NULL)
-            {
-                temp->PrintCourseData();
-                temp=temp->next;
-            }
+            temp->PrintCourseData();
+            temp = temp->next;
         }
     }
-    void SearchList(std::string value)
+}
+void LinkedList::SearchList(std::string value)
+{
+    loc = first;
+    ploc = NULL;
+    while (loc != NULL && loc->data.name > value)
     {
-        loc=first;
-        ploc=NULL;
-        while(loc!=NULL && loc->data.name>value)
-        {
-            ploc=loc;
-            loc=loc->next;
-        }
-        if(loc!=NULL && loc->data.name!=value)
-        {
-            loc=NULL;
-        }
+        ploc = loc;
+        loc = loc->next;
     }
-    void insertFront()
+    if (loc != NULL && loc->data.name != value)
     {
-        Course* newCourse= new Course;
-        newCourse->InsertUserCourseData();
-        if(isEmpty())
-        {
-            first=newCourse;
-            last=newCourse;
-            newCourse->next=NULL;
-        }
+        loc = NULL;
+    }
+}
+void LinkedList::insertFront()
+{
+    Course *newCourse = new Course;
+    newCourse->InsertUserCourseData();
+    if (isEmpty())
+    {
+        first = newCourse;
+        last = newCourse;
+        newCourse->next = NULL;
+    }
+    else
+    {
+        newCourse->next = first;
+        first = newCourse;
+    }
+    length++;
+}
+
+void LinkedList::insert(Course *obj)
+{
+    obj->data.course_id = ++obj->data.courseCount; 
+    if (isEmpty())
+    {
+        first = obj;
+        last = obj;
+    }
+    else
+    {
+        last->next = obj;
+        last = obj;
+    }
+    length++;
+}
+
+void LinkedList::insertNewNode(std::string value)
+{
+    Course *newCourse = new Course;
+    newCourse->InsertUserCourseData();
+    SearchList(value);
+    if (loc == NULL)
+    {
+        if (ploc == NULL)
+            insertFront();
         else
         {
-            newCourse->next=first;
-            first=newCourse;
+            newCourse->next = ploc->next;
+            ploc->next = newCourse;
+            if (ploc == last)
+            {
+                last = newCourse;
+            }
         }
         length++;
     }
-    void insert(std::string value)
+    else
     {
-        Course* newCourse= new Course;
-        newCourse->InsertUserCourseData();
+        std::cout << "Duplication of courses is not allowed" << std::endl;
+    }
+}
+void LinkedList::insertFile(std::string s)
+{
+    std::ifstream myFile;
+    myFile.open(s);
+    int len = 10;
+    int count = 0;
+    std::string str = "";
+    std::string line, str1, str2;
+    while (myFile.good())
+    {
+        Course *newNode = new Course();
+        getline(myFile, line, ',');
+        newNode->data.name = line;
+
+        getline(myFile, line, ',');
+        newNode->data.url = line;
+
+        getline(myFile, line, ',');
+        if (line.compare("TRUE"))
+            newNode->data.isPaid = true;
+        else
+            newNode->data.isPaid = false;
+
+        getline(myFile, line, ',');
+        newNode->data.price = stringToint(line);
+
+        getline(myFile, line, ',');
+        newNode->data.subscribers = stringToint(line);
+
+        getline(myFile, line, ',');
+        newNode->data.difficulty_id = indexLevel(line);
+
+        getline(myFile, line, ',');
+        newNode->data.duration = stringToint(line);
+
+        getline(myFile, line, ',');
+        str1 = line.substr(0, 4);
+        newNode->data.publish_year = stringToint(str1);
+
+        getline(myFile, line, '\n');
+        newNode->data.Category_id = indexSubject(line);
+        this->insert(newNode);
+    }
+}
+void LinkedList::deleteNode(std::string value)
+{
+    if (!isEmpty())
+    {
         SearchList(value);
-        if(loc==NULL)
+        if (loc != NULL)
         {
-            if(ploc==NULL)
-            insertFront();
-            else
+            if (first == last)
             {
-                newCourse->next=ploc->next;
-                ploc->next=newCourse;
-                if (ploc==last)
-                {
-                    last=newCourse;
-                }
+                first = NULL;
+                last = NULL;
             }
-            length++;
-        }
-        else
-        {
-            std::cout<<"Duplication of courses is not allowed"<<std::endl;
-        }
-    }
-    void insertFile()
-    {
-        //<<<<<<<<<<<<<<<<<<<<<<<<
-        //<<<<<<<<<<<<<<<<<<<<<<<<  code to be inserted here
-        //<<<<<<<<<<<<<<<<<<<<<<<< 
-    }
-    void deleteNode(std::string value)
-    {
-        if(!isEmpty())
-        {
-            SearchList(value);
-            if(loc!=NULL)
+            else if (ploc == NULL)
             {
-                if(first==last)
-                {
-                    first=NULL;
-                    last=NULL;
-                }
-                else if(ploc==NULL)
-                {
-                    first=first->next;
-                    delete loc;
-                    loc=first;
-                }
-                else if(loc->next)
-                {
-                    delete last;
-                    last=ploc;
-                    last->next=NULL;
-                }
-                else
-                {
-                    ploc->next=loc->next;
-                    delete loc;
-                }
-                length--;
+                first = first->next;
+                delete loc;
+                loc = first;
+            }
+            else if (loc->next)
+            {
+                delete last;
+                last = ploc;
+                last->next = NULL;
             }
             else
             {
-                std::cout<<"Value Not Found"<<std::endl;
+                ploc->next = loc->next;
+                delete loc;
             }
+            length--;
         }
         else
-        std::cout<<"No courses are in the structure"<<std::endl;
-    }
-    void destroy()
-    {
-        Course* temp;
-        while(first!=NULL)
         {
-            temp=first;
-            first=first->next;
-            delete temp;
+            std::cout << "Value Not Found" << std::endl;
         }
-        last=NULL;
     }
-};
+    else
+        std::cout << "No courses are in the structure" << std::endl;
+}
+void LinkedList::destroy()
+{
+    Course *temp;
+    while (first != NULL)
+    {
+        temp = first;
+        first = first->next;
+        delete temp;
+    }
+    last = NULL;
+}
+
+int LinkedList::indexSubject(std::string str)
+{
+    if (str.compare("Business Finance"))
+        return 1;
+    else if (str.compare("Graphic Design"))
+        return 2;
+    else if (str.compare("Musical Instruments"))
+        return 3;
+    else if (str.compare("Web Development"))
+        return 4;
+    else
+        return 5;
+}
+
+int LinkedList::stringToint(std::string str)
+{
+    int x;
+    std::stringstream val(str);
+    val >> x;
+    return x;
+}
+
+int LinkedList::indexLevel(std::string str)
+{
+    if (str.compare("Beginner Level"))
+        return 1;
+    else if (str.compare("Intermediate Level"))
+        return 2;
+    else if (str.compare("Expert Level"))
+        return 3;
+    else
+        return 4;
+}
--- datatype.hpp
+++ datatype.hpp
@@ -1,6 +1,85 @@
+#pragma once
 #include <iostream>
 #include <string>
 
+class Course_data
+{
+public:
+    std::string name;
+    std::string url;
+    int Category_id;   // using h
+    int difficulty_id; // Beginner, advanced etc
+    int price;
+    int subscribers;
+    int course_id;
+    int publish_year;
+    float duration;
+    bool isPaid;
+    static int courseCount;
+};
+
+int Course_data::courseCount = 0;
+
+class Course
+{
+public:
+    int index;
+    Course *next;  //for linked list use
+    Course *lrank; //for heap
+    Course *rrank;
+    Course_data data;
+    void PrintCourseData() //function to print the data of the course
+    {
+        std::cout << "Course_ID: " << data.course_id << std::endl;
+        std::cout << "Course title: " << data.name << std::endl;
+        std::cout << "Link: " << data.url << std::endl;
+        std::cout << "Category_ID: " << data.Category_id << std::endl;
+        std::cout << "Price" << data.price << std::endl;
+        std::cout << "Number of students enrolled: " << data.subscribers << std::endl;
+        std::cout << "Year published: " << data.publish_year << std::endl;
+        std::cout << "Hours to complete: " << data.duration << std::endl;
+        std::cout << "isPaid: " << data.isPaid << std::endl<< std::endl;
+    }
+    void InsertUserCourseData() //function to insert the data into the course
+    {
+        std::cin >> data.name;
+        std::cin >> data.url;
+        std::cin >> data.Category_id;
+        std::cin >> data.price;
+        std::cin >> data.subscribers;
+        std::cin >> data.course_id;
+        std::cin >> data.publish_year;
+        std::cin >> data.duration;
+        if (data.price != 0)
+            data.isPaid == false;
+    }
+    void InsertFromFile()
+    {
+    }
+};
+
+class LinkedList
+{
+public:
+    Course *first;
+    Course *last;
+    int length;
+    Course *ploc;
+    Course *loc;
+    LinkedList();
+    bool isEmpty();
+    void printList();
+    void SearchList(std::string); //searches through list linearly
+    void insertFront();
+    void insertNewNode(std::string);
+    void insert(Course *);
+    void insertFile(std::string);
+    void deleteNode(std::string);
+    void destroy(); //basic linked list functionalities
+    int indexSubject(std::string);
+    int stringToint(std::string);
+    int indexLevel(std::string);
+};
 
 class Category
 {
@@ -10,83 +89,7 @@ public:
     Course *Tnext;
 };
 
-class Course_data
-{
-public:
-    std::string name;
-    std::string url;
-    int Category_id; //using h
-    int price;
-    int subscribers;
-    int course_id;
-    int publish_year;
-    float duration;
-    bool isPaid;
-};
-
-class Course
-{
-public:
-   
-    int index;
-    Course *next;  //for linked list use
-    Course *lrank; //for heap
-    Course *rrank;
-    Course_data data;
-    void PrintCourseData()//function to print the data of the course
-    {
-        std::cout<< data.name <<std::endl;
-        std::cout<< data.url<<std::endl;
-        std::cout<< data.Category_id <<std::endl;
-        std::cout<< data.price <<std::endl;
-        std::cout<< data.subscribers <<std::endl;
-        std::cout<< data.course_id <<std::endl;
-        std::cout<< data.publish_year<<std::endl;
-        std::cout<< data.duration<<std::endl;
-        std::cout<< data.isPaid<<std::endl;
-    }
-    void InsertUserCourseData()//function to insert the data into the course
-    {
-        std::cin>>data.name;
-        std::cin>>data.url;
-        std::cin>>data.Category_id;
-        std::cin>>data.price;
-        std::cin>>data.subscribers;
-        std::cin>>data.course_id;
-        std::cin>>data.publish_year;
-        std::cin>>data.duration;
-        if(data.price!=0)
-        data.isPaid==false;
-    }
-    void InsertFromFile()
-    {
-
-    }
-};
-
-
-
-class LinkedList
-{
-public:
-    Course* first;
-    Course* last;
-    int length;
-    Course* ploc;
-    Course* loc;
-    LinkedList();
-    bool isEmpty();
-    void printList();
-    void SearchList(std::string);//searches through list linearly
-    void insertFront();
-    void insert();
-    void insertFile();
-    void deleteNode();
-    void destroy();//basic linked list functionalities
-};
-
-void CreateResList()//When a word is searched, a smaller linked list 
+//void CreateResList() //When a word is searched, a smaller linked list
 //of the words that are hit in the search will be returned
-{
-
-}
+//{
+//}
