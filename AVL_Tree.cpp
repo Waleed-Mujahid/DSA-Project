561ef0e2effc560ef39fe9857b07e55c0749ddae
Waleed Mujahid <mmujahid.bscs20seecs@seecs.edu.pk>
2021-12-24 14:09:16 +0500

    AVL implemnetation for Dataset

---
+++ AVL_Tree.cpp
@@ -0,0 +1,479 @@
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include "datatype.hpp"
+
+using namespace std;
+
+bool stack::isEmpty()
+{
+    return start == NULL;
+}
+
+void stack::push(Course* node)
+{
+    stackNode* NewNode = new stackNode();
+    NewNode->ptr = node;
+    NewNode->next = start;
+    start = NewNode;
+}
+
+Course* stack::pop()
+{
+    if (isEmpty())
+        return nullptr;
+
+    loc = start;
+    Course* value;
+    value = start->ptr;
+    start = start->next;
+    delete loc;
+    return value;
+}
+
+void AVL_Tree::ballanceAVL(Course* current, int depth_difference)
+{
+    
+    if (depth_difference == 0 || depth_difference == -1 || depth_difference == 1)
+        return; // AVL tree is already ballanced
+
+    else if (depth_difference > 1) // Left subtree is more deep than right subtree of CURRENT NODE
+    {
+        
+        loc = current->LeftChild;
+        // Checks if single rotaion required or double
+        if (depth_of_tree(loc->RightChild) > depth_of_tree(loc->LeftChild))
+            leftRightRotaion(current);
+        else
+            rightRotation(current);
+    }
+
+    else // Right subtree is more deep than left subtree of CURRENT NODE
+    {
+        
+        loc = current->RightChild;
+        // Checks if single rotaion required or double
+        if (depth_of_tree(loc->LeftChild) > depth_of_tree(loc->RightChild))
+        {
+            rightLeftRotaion(current);
+        }
+        else
+        {
+            leftRotation(current);
+        }
+    }
+}
+
+void AVL_Tree::leftRightRotaion(Course* x)
+{
+    leftRotation(x->LeftChild);
+    rightRotation(x);
+}
+
+void AVL_Tree::rightLeftRotaion(Course* x)
+{
+    rightRotation(x->RightChild);
+    leftRotation(x);
+}
+
+void AVL_Tree::leftRotation(Course* x)
+{
+    
+    search(x); // Search for node X to get its logical predecessor
+    Course* y = x->RightChild;
+
+    // Put y in the place of X
+    if (ploc == NULL)
+        root = y;
+    else if (ploc->RightChild == x)
+        ploc->RightChild = y;
+    else
+        ploc->LeftChild = y;
+
+    // Left rotation
+    x->RightChild = y->LeftChild;
+    y->LeftChild = x;
+}
+
+void AVL_Tree::rightRotation(Course* x)
+{
+    search(x); // Search for node X to get its logical predecessor
+    Course* y = x->LeftChild;
+
+    // Put y in the place of X
+    if (ploc == NULL)
+        root = y;
+    else if (ploc->RightChild == x)
+        ploc->RightChild = y;
+    else
+        ploc->LeftChild = y;
+
+    // Right rotation
+    x->LeftChild = y->RightChild;
+    y->RightChild = x;
+}
+
+// Deletes entire BST
+void AVL_Tree::destroyTree(Course* current)
+{
+    if (current != NULL) // Traverses tree in Post order
+    {
+        destroyTree(current->LeftChild);
+        destroyTree(current->RightChild);
+
+        if (current == root) // Root case
+            root = NULL;
+
+        current->LeftChild = NULL; // Sets child nodes to null
+        current->RightChild = NULL;
+        delete current; // Deletes node
+    }
+}
+
+bool AVL_Tree::IsEmpty()
+{
+    //Returns true if no node is present(Tree is Empty)
+    return root == NULL;
+}
+
+void AVL_Tree::search(Course* obj)
+{
+    string str = obj->data.name;
+    if (IsEmpty())
+        return;
+
+    
+    //Initialize loc and ploc
+    ploc = NULL;
+    loc = root;
+    string temp_Comparison;
+    s.push(loc);
+
+
+    //Traverse Tree to the end node or untill the value is found
+    while (loc != NULL && loc->data.name != str)
+    {
+        ploc = loc;
+        temp_Comparison = loc->data.name;
+
+        //If value is greater than the value in current node go to right
+        if (str > temp_Comparison)
+            loc = loc->RightChild;        
+        
+        else        //Otherwise go to the left
+            loc = loc->LeftChild;
+
+        s.push(loc);
+    }
+}
+
+void AVL_Tree::Insert(Course* obj)
+{
+    string str = obj->data.name;
+    obj->data.course_id = ++len;
+
+    search(obj); //Search Position for insertion
+    //If value is not found then insert value
+    if (loc == NULL)
+    {
+        //If tree is empty
+        if (ploc == NULL)
+            root = obj; //Newnode is pointed by root
+
+        else
+        {
+            //Use ploc to insert the value
+            if (str > ploc->data.name)
+                ploc->RightChild = obj;
+            else
+                ploc->LeftChild = obj;
+        }
+
+        Course* temp = NULL;
+        int depth_Diff = 0;        
+        
+        while (!s.isEmpty())
+        {
+            temp = s.pop();
+            if (temp != NULL)
+               depth_Diff = - depth_of_tree(temp->RightChild) + depth_of_tree(temp->LeftChild);
+
+            if (depth_Diff > 1 || depth_Diff < -1)
+                break;
+        }
+        ballanceAVL(temp, depth_Diff);
+    }
+}
+
+void AVL_Tree::PreOrder(Course* temp)
+{
+
+    if (temp == NULL)
+    {
+        //Base Case
+        return;
+    }
+    //Print node
+    temp->PrintCourseData();
+    //Go to the left child recursively
+    PreOrder(temp->LeftChild);
+    //Then go to the right child recursively
+    PreOrder(temp->RightChild);
+}
+
+void AVL_Tree::InOrder(Course* temp)
+{
+
+    if (IsEmpty())
+        cout << endl
+        << "Tree is empty." << endl;
+
+    if (temp == NULL)
+    {
+        //Base Case
+        return;
+    }
+    //Go to the left child recursively
+    InOrder(temp->LeftChild);
+    //Then print node
+    temp->PrintCourseData();
+    //Then go to the right child recursively
+    InOrder(temp->RightChild);
+}
+
+void AVL_Tree::PostOrder(Course* temp)
+{
+    if (temp == NULL)
+    {
+        //Base Case
+        return;
+    }
+    //Go to the left child recursively
+    PostOrder(temp->LeftChild);
+    //Then go to the right child recursively
+    PostOrder(temp->RightChild);
+    //Then print node
+    temp->PrintCourseData();
+}
+
+int AVL_Tree::height_of_tree(Course* temp)
+{
+    if (temp == NULL)
+    {
+        //If traversal goes beyond leaf nodes
+        return -1;
+    }
+    else
+    {
+        //return 1 + max height of left or right child
+        return 1 + max(height_of_tree(temp->LeftChild), height_of_tree(temp->RightChild));
+    }
+}
+
+int AVL_Tree::depth_of_tree(Course* temp)
+{
+    if (temp == NULL)
+    {
+        //If traversal goes beyond leaf nodes
+        return -1;
+    }
+    else
+    {
+        //return 1 + max height of left or right child
+        return 1 + max(depth_of_tree(temp->LeftChild), depth_of_tree(temp->RightChild));
+    }
+}
+
+void AVL_Tree::deleteNode(Course* obj)
+{
+    string str = obj->data.name;
+    search(obj);
+    if (loc != NULL)
+    {
+        if (loc->LeftChild == NULL && loc->RightChild == NULL)
+        {
+
+            if (ploc == NULL)
+                root = NULL;
+            else if (ploc->LeftChild == loc)
+                ploc->LeftChild = NULL;
+            else
+                ploc->RightChild = NULL;
+
+            delete loc;
+        }
+        else if (loc->LeftChild == NULL && loc->RightChild != NULL)
+        {
+            if (ploc == NULL)
+                root = loc->RightChild;
+            else if (ploc->LeftChild == loc)
+                ploc->LeftChild = loc->RightChild;
+            else
+                ploc->RightChild = loc->RightChild;
+
+            delete loc;
+        }
+        else if (loc->LeftChild != NULL && loc->RightChild == NULL)
+        {
+            if (ploc == NULL)
+                root = loc->LeftChild;
+            else if (ploc->LeftChild == loc)
+                ploc->LeftChild = loc->LeftChild;
+            else
+                ploc->RightChild = loc->LeftChild;
+
+            delete loc;
+        }
+        else
+        {
+            Course* tploc = ploc;
+            Course* tloc = loc;
+            ploc = loc;
+            loc = loc->LeftChild;
+
+            while (loc->RightChild != NULL)
+            {
+                ploc = loc;
+                loc = loc->RightChild;
+            }
+
+            if (tloc->LeftChild == loc)
+                tloc->LeftChild = loc->LeftChild;
+            else if (loc->LeftChild == NULL)
+                ploc->RightChild = NULL;
+            else
+                ploc->RightChild = loc->LeftChild;
+
+            loc->LeftChild = tloc->LeftChild;
+            loc->RightChild = tloc->RightChild;
+
+            if (tploc == NULL)
+                root = loc;
+            else if (tploc->LeftChild == tloc)
+            {
+                tploc->LeftChild = loc;
+            }
+            else
+            {
+                tploc->RightChild = loc;
+            }
+            delete tloc;
+        }
+
+        Course* temp = NULL;
+        int depth_Diff = 0;
+        s.pop(); // Deleted node stored in stack;
+        while (!s.isEmpty())
+        {
+            temp = s.pop();
+            if (temp != NULL)
+            {
+                depth_Diff = depth_of_tree(temp->LeftChild) - depth_of_tree(temp->RightChild);
+            }
+            if (depth_Diff > 1 || depth_Diff < -1)
+                break;
+        }
+        ballanceAVL(temp, depth_Diff);
+    }
+    else
+    {
+        cout << "Value not found. Deleteion can not be done." << endl;
+    }
+}
+
+int AVL_Tree::indexSubject(std::string str)
+{
+    if (str.compare("Business Finance"))
+        return 1;
+    else if (str.compare("Graphic Design"))
+        return 2;
+    else if (str.compare("Musical Instruments"))
+        return 3;
+    else if (str.compare("Web Development"))
+        return 4;
+    else
+        return 5;
+}
+
+int AVL_Tree::stringToint(std::string str)
+{
+    int x;
+    std::stringstream val(str);
+    val >> x;
+    return x;
+}
+
+int AVL_Tree::indexLevel(std::string str)
+{
+    if (str.compare("Beginner Level"))
+        return 1;
+    else if (str.compare("Intermediate Level"))
+        return 2;
+    else if (str.compare("Expert Level"))
+        return 3;
+    else
+        return 4;
+}
+
+void AVL_Tree::insertFile(std::string s)
+{
+    std::ifstream myFile;
+    myFile.open(s);
+    string str = "";
+    string line, str1, str2;
+    int flag;
+    while (myFile.good())
+    {
+        Course* newNode = new Course();
+        flag = 1;
+        line = "";
+        getline(myFile, line, ',');
+
+        for (int i = 0; i < line.length(); i++)
+        {
+            if (isalnum(line[i]) == 0 && isspace(line[i]) == 0 && ispunct(line[i]) == 0)
+            {
+                flag = 0;
+                break;
+            }
+        }
+
+        if (flag)
+            newNode->data.name = line;
+        else
+        {
+            getline(myFile, line, '\n');
+        }
+        newNode->data.name = line;
+
+        getline(myFile, line, ',');
+        newNode->data.url = line;
+
+        getline(myFile, line, ',');
+        if (line.compare("TRUE"))
+            newNode->data.isPaid = true;
+        else
+            newNode->data.isPaid = false;
+
+        getline(myFile, line, ',');
+        newNode->data.price = stringToint(line);
+
+        getline(myFile, line, ',');
+        newNode->data.subscribers = stringToint(line);
+
+        getline(myFile, line, ',');
+        newNode->data.difficulty_id = indexLevel(line);
+
+        getline(myFile, line, ',');
+        newNode->data.duration = stringToint(line);
+
+        getline(myFile, line, ',');
+        str1 = line.substr(0, 4);
+        newNode->data.publish_year = stringToint(str1);
+
+        getline(myFile, line, '\n');
+        newNode->data.Category_id = indexSubject(line);
+        Insert(newNode);
+    }
+}
--- datatype.hpp
+++ datatype.hpp
@@ -2,6 +2,11 @@
 #include <iostream>
 #include <string>
 
+// Creates various data types used to Implement AVL trees for our Dataset
+// Course_Data stores all the data of one Course
+// Course is a node which contains data and next pointer
+// AVL trees are used to store Courses
+// Stack is used to balance avl trees
 class Course_data
 {
 public:
@@ -15,30 +20,29 @@ public:
     int publish_year;
     float duration;
     bool isPaid;
-    static int courseCount;
 };
-
-int Course_data::courseCount = 0;
 
 class Course
 {
 public:
     int index;
-    Course *next;  //for linked list use
-    Course *lrank; //for heap
-    Course *rrank;
+    Course* next;  //for linked list use
+    Course* lrank; //for heap
+    Course* rrank;
     Course_data data;
+    Course* LeftChild;
+    Course* RightChild;
     void PrintCourseData() //function to print the data of the course
     {
         std::cout << "Course_ID: " << data.course_id << std::endl;
         std::cout << "Course title: " << data.name << std::endl;
         std::cout << "Link: " << data.url << std::endl;
         std::cout << "Category_ID: " << data.Category_id << std::endl;
-        std::cout << "Price" << data.price << std::endl;
+        std::cout << "Price: " << data.price << std::endl;
         std::cout << "Number of students enrolled: " << data.subscribers << std::endl;
         std::cout << "Year published: " << data.publish_year << std::endl;
         std::cout << "Hours to complete: " << data.duration << std::endl;
-        std::cout << "isPaid: " << data.isPaid << std::endl<< std::endl;
+        std::cout << "isPaid: " << data.isPaid << std::endl << std::endl;
     }
     void InsertUserCourseData() //function to insert the data into the course
     {
@@ -53,43 +57,98 @@ public:
         if (data.price != 0)
             data.isPaid == false;
     }
-    void InsertFromFile()
-    {
-    }
+
 };
 
 class LinkedList
 {
 public:
-    Course *first;
-    Course *last;
+    Course* first;
+    Course* last;
     int length;
-    Course *ploc;
-    Course *loc;
+    Course* ploc;
+    Course* loc;
     LinkedList();
     bool isEmpty();
     void printList();
     void SearchList(std::string); //searches through list linearly
     void insertFront();
     void insertNewNode(std::string);
-    void insert(Course *);
-    void insertFile(std::string);
+    void insert(Course*);
     void deleteNode(std::string);
     void destroy(); //basic linked list functionalities
     int indexSubject(std::string);
     int stringToint(std::string);
     int indexLevel(std::string);
+    void insertFile(std::string);
 };
 
-class Category
+class stackNode // node to implement Stack datastructure
 {
 public:
-    int index;
-    std::string t_name;
-    Course *Tnext;
+    Course* ptr;
+    stackNode* next;
+    stackNode()
+    {
+        ptr = NULL;
+        next = NULL;
+    }
 };
 
-//void CreateResList() //When a word is searched, a smaller linked list
-//of the words that are hit in the search will be returned
-//{
-//}
+class stack
+{
+public:
+    stackNode* start;
+    stackNode* loc;
+    stack()
+    {
+        start = NULL;
+        loc = NULL;
+    }
+
+    bool isEmpty();
+    Course* pop();
+    void push(Course*);
+};
+
+class AVL_Tree
+{
+public:
+    stack s; // Used to ballance AVL
+    Course* root;
+    Course* loc;
+    Course* ploc;
+    int len;
+    AVL_Tree()
+    {
+        len = 0;
+        root = NULL;
+        loc = NULL;
+        ploc = NULL;
+    }
+    bool IsEmpty();
+    void search(Course*);
+    void Insert(Course*);
+    void PreOrder(Course*);
+    void InOrder(Course*);
+    void PostOrder(Course*);
+    int height_of_tree(Course*);
+    void destroyTree(Course*);
+    void deleteNode(Course*);
+
+    // functions to ballance AVL tree
+
+    void leftRotation(Course*);
+    void rightRotation(Course*); // Rotation functions
+    void leftRightRotaion(Course*);
+    void rightLeftRotaion(Course*);
+
+    int depth_of_tree(Course*); // Returns depth of a node. used for ballancing an AVL tree
+    void ballanceAVL(Course*, int);
+
+    // File reading functions
+    int indexSubject(string);
+    int stringToint(string);
+    int indexLevel(string);
+    void insertFile(string);
+};
